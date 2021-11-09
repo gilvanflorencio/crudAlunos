7e71470f6fa250a6b2539cc4576fb09d252ef065
GILVANSANTOS <gilvanf28@gmail.com>
2021-11-08 22:34:26 -0300

    corrigindo errors async migration A,M

--- src/database/migrations/20211106233714-mudar-email-aluno-unique.js
+++ src/database/migrations/20211106233714-mudar-email-aluno-unique.js
@@ -9,5 +9,5 @@ module.exports = {
     },
   ),
 
-  down: () => {},
+  down: async () => {},
 };
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -20,7 +20,7 @@ export default class Aluno extends Model {
         validate: {
           len: {
             args: [3, 255],
-            msg: 'Obrenome Precisa ter entre 3 e 255 caracteres.',
+            msg: 'Sobrenome Precisa ter entre 3 e 255 caracteres.',
           },
         },
       },
